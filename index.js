b0cba01544eb03c25008c4cee0d0e095607ba38b
Dmitro1411 <priv111vid@gmail.com>
2023-08-24 22:22:22 +0300

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 function stringToNumber(str) {
   // Використовуємо Number() для перетворення рядка в число
   // Повертаємо отримане число
+  console.log(typeof str);
+  console.log(typeof Number(str));
+  return (stringToNumber = Number(str));
 }
 
 console.log("Завдання 1 ====================================");
@@ -12,6 +15,20 @@ console.log('stringToNumber("42")', stringToNumber("42")); // Виведе 42
 
 function isAlmostSame(num1, num2) {
   // Визначаємо, чи є різниця між двома числами меншою або рівною EPSILON
+  console.log(num1 - num2, num2 - num1, Number.EPSILON);
+  {
+    return num1 - num2 <= Number.EPSILON;
+  }
+
+  //   {
+  //     if (num1 - num2 < Number.EPSILON < num2 - num1) {
+  //       return `num1 and num2 Практически равные`;
+  //     } else if (num1 - num2 === Number.EPSILON) {
+  //       return `num1 and num2 равны`;
+  //     } else {
+  //       return `num1 and num2 Не равны`;
+  //     }
+  //   }
 }
 
 console.log("Завдання 2 ====================================");
@@ -21,6 +38,10 @@ console.log("isAlmostSame(0.1 + 0.2, 0.3)", isAlmostSame(0.1 + 0.2, 0.3)); // В
 
 function isSafeInteger(num) {
   // Перевіряємо, чи є число меншим або рівним MAX_SAFE_INTEGER і більшим або рівним MIN_SAFE_INTEGER
+  console.log(Number.MIN_SAFE_INTEGER);
+  console.log(Number.MAX_SAFE_INTEGER);
+  console.log(num);
+  return Number.MAX_SAFE_INTEGER >= num <= Number.MIN_SAFE_INTEGER;
 }
 
 console.log("Завдання 3 ====================================");
@@ -33,6 +54,7 @@ console.log(
 
 function isUnsafeInteger(num) {
   // Використовуємо логічну оператор НЕ (!), щоб отримати протилежну відповідь від функції isSafeInteger()
+  return num !== isSafeInteger;
 }
 
 console.log("Завдання 4 ====================================");
@@ -45,6 +67,7 @@ console.log(
 
 function isTooLarge(num) {
   // Перевіряємо, чи є число більшим за MAX_VALUE
+  return num > Number.MAX_VALUE;
 }
 
 console.log("Завдання 5 ====================================");
@@ -57,6 +80,8 @@ console.log(
 
 function isAlmostZero(num) {
   // Перевіряємо, чи є число більше 0, але все ще менше за Number.MIN_VALUE
+
+  return num > 0 && num < Number.MIN_VALUE;
 }
 
 console.log("Завдання 6 ====================================");
@@ -70,6 +95,7 @@ console.log("isAlmostZero(Number.MIN_VALUE)", isAlmostZero(Number.MIN_VALUE)); /
 
 function checkIsInteger(num) {
   // Використовуємо вбудовану функцію Number.isInteger(), щоб перевірити, чи є значення цілим числом
+  return Number.isInteger(num);
 }
 
 console.log("Завдання 7 ====================================");
@@ -79,6 +105,7 @@ console.log("checkIsInteger(42.5)", checkIsInteger(42.5)); // Виведе false
 
 function checkIsSafeInteger(num) {
   // Використовуємо вбудовану функцію Number.isSafeInteger(), щоб перевірити, чи є значення безпечним цілим числом
+  return Number.isSafeInteger(num);
 }
 
 console.log("Завдання 8 ====================================");
@@ -91,6 +118,8 @@ console.log(
 
 function convertToExponential(num) {
   // Використовуємо метод toExponential(), щоб конвертувати число в рядок з експоненційним представленням
+
+  return num.toExponential();
 }
 
 console.log("Завдання 9 ====================================");
@@ -100,6 +129,7 @@ console.log("convertToExponential(42)", convertToExponential(42)); // Вивед
 
 function convertToFixed(num, precision) {
   // Використовуємо метод toFixed(), щоб конвертувати число в рядок з фіксованою кількістю знаків після коми
+  return num.toFixed(precision);
 }
 
 console.log("Завдання 10 ====================================");
@@ -109,6 +139,8 @@ console.log("convertToFixed(42.9876, 2)", convertToFixed(42.9876, 2)); // Вив
 
 function convertToString(num) {
   // Використовуємо метод toString(), щоб конвертувати число в рядок
+  console.log(typeof num.toString());
+  return num.toString();
 }
 
 console.log("Завдання 11 ====================================");
@@ -118,6 +150,7 @@ console.log("convertToString(42)", convertToString(42)); // Виведе "42"
 
 function convertToPrecision(num, precision) {
   // Використовуємо метод toPrecision(), щоб округлити число до вказаної довжини
+  return num.toPrecision(precision);
 }
 
 console.log("Завдання 12 ====================================");
